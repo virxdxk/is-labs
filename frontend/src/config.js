de5dbc18a80a70a3508a30c428f329fe44493153
virxdxk <eliseevkonstantin306@gmail.com>
2025-11-08 15:59:18 +0300

    feat(front): make read api port from .env

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,11 +1,13 @@
 const MODE = 'prod'
 
+const PORT = import.meta.env.API_PORT || 8080
+
 export let API_BASE_PATH;
 export let WS_BASE_PATH;
 
 if(MODE === 'dev') {
-    API_BASE_PATH = 'http://localhost:8080/is-labs-1.0'
-    WS_BASE_PATH = 'ws://localhost:8080/is-labs-1.0'
+    API_BASE_PATH = `http://localhost:${PORT}/`
+    WS_BASE_PATH = `ws://localhost:${PORT}/`
 }
 
 if(MODE === 'prod') {
