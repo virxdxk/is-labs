18f97c75f91a3f057e3cdac33b5998bbeaad3a80
virxdxk <eliseevkonstantin306@gmail.com>
2025-11-09 13:18:03 +0300

    fix: route websoccket

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,16 +1,2 @@
-const MODE = 'prod'
-
-const PORT = import.meta.env.API_PORT || 8080
-
-export let API_BASE_PATH;
-export let WS_BASE_PATH;
-
-if(MODE === 'dev') {
-    API_BASE_PATH = `http://localhost:${PORT}/`
-    WS_BASE_PATH = `ws://localhost:${PORT}/`
-}
-
-if(MODE === 'prod') {
-    API_BASE_PATH = 'api'
-    WS_BASE_PATH = 'ws'
-}
+export let API_BASE_PATH = `http://localhost:9696/app`;
+export let WS_BASE_PATH = `ws://localhost:9696/app`;
